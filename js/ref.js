1564fbf48ba12947f715ca79bb6f3f3030eeef55
James <120222231+jbenedi@users.noreply.github.com>
2025-07-28 20:44:09 -0400

    Update and rename ref.js to ref.js

---
+++ js/ref.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { Octokit } = require('@octokit/rest');
+
+(async () => {
+  try {
+    const token = core.getInput('token');
+    const octokit = new Octokit({ auth: token });
+
+    const { owner, repo } = github.context.repo;
+    const issueNumber = github.context.payload.issue.number;
+
+    function encodeIssueId(id) {
+      return Buffer.from(String(id)).toString('base64').replace(/=+$/, '').replace(/\+/g, '-').replace(/\//g, '_');
+    }
+
+    const shortCode = encodeIssueId(issueNumber + 10);
+    const shortUrl = `https://jbenedi.work/link/${shortCode}`;
+
+    await octokit.issues.createComment({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      body: shortUrl
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();
